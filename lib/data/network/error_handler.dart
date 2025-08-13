f64eae1d333aaaf5a6e89897a2076a2568d9bf42
Amr-B <amr.b4dev@gmail.com>
2025-08-13 15:26:53 +0300

    created handling cases in error handler

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
+import 'failure.dart';
+
 enum DataSource {
   SUCCESS,
   NO_CONTNET,
@@ -14,6 +16,69 @@ enum DataSource {
   CACHE_ERROR,
   SEND_TIMEOUT,
   NO_INTERNET_CONNECTION,
+}
+
+// > cases
+extension DataSourceExtension on DataSource {
+  Failure getFailure() {
+    switch (this) {
+      // SUCCESS
+      case DataSource.SUCCESS:
+        return Failure(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+
+      // NO_CONTENT
+      case DataSource.NO_CONTNET:
+        return Failure(ResponseCode.NO_CONTNET, ResponseMessage.NO_CONTNET);
+
+      // BAD_REQUEST
+      case DataSource.BAD_REQUEST:
+        return Failure(ResponseCode.BAD_REQUEST, ResponseMessage.BAD_REQUEST);
+
+      // FORBIDDEN
+      case DataSource.FORBIDDEN:
+        return Failure(ResponseCode.FORBIDDEN, ResponseMessage.FORBIDDEN);
+
+      // UNAUTHORISED
+      case DataSource.UNAUTHORISED:
+        return Failure(ResponseCode.UNAUTHORISED, ResponseMessage.UNAUTHORISED);
+
+      // NOT_FOUND
+      case DataSource.NOT_FOUND:
+        return Failure(ResponseCode.NOT_FOUND, ResponseMessage.NOT_FOUND);
+
+      // INTERNAL_SERVER_ERROR
+      case DataSource.INTERNAL_SERVER_ERROR:
+        return Failure(ResponseCode.INTERNAL_SERVER_ERROR,
+            ResponseMessage.INTERNAL_SERVER_ERROR);
+
+      // CONNECT_TIMEOUT
+      case DataSource.CONNECT_TIMEOUT:
+        return Failure(
+            ResponseCode.CONNECT_TIMEOUT, ResponseMessage.CONNECT_TIMEOUT);
+
+      // CANCEL
+      case DataSource.CANCEL:
+        return Failure(ResponseCode.CANCEL, ResponseMessage.CANCEL);
+
+      // RECEIVE_TIMEOUT
+      case DataSource.RECIEVE_TIMEOUT:
+        return Failure(
+            ResponseCode.RECIEVE_TIMEOUT, ResponseMessage.RECIEVE_TIMEOUT);
+
+      // CACHE_ERROR
+      case DataSource.CACHE_ERROR:
+        return Failure(ResponseCode.CACHE_ERROR, ResponseMessage.CACHE_ERROR);
+
+      // SEND_TIMEOUT
+      case DataSource.SEND_TIMEOUT:
+        return Failure(ResponseCode.SEND_TIMEOUT, ResponseMessage.SEND_TIMEOUT);
+
+      // NO_INTERNET_CONNECTION
+      case DataSource.NO_INTERNET_CONNECTION:
+        return Failure(ResponseCode.NO_INTERNET_CONNECTION,
+            ResponseMessage.NO_INTERNET_CONNECTION);
+    }
+  }
 }
 
 class ResponseCode {
