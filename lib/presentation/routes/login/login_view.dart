fb0fa33e35a56b227284041f066b2cf7a968db31
Amr-B <amr.b4dev@gmail.com>
2025-08-18 14:08:29 +0300

    binding login view model to login view

--- lib/presentation/routes/login/login_view.dart
+++ lib/presentation/routes/login/login_view.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: unused_field, prefer_final_fields
+
+import 'package:advanced_flutter_tutorial/presentation/auth/login/viewmodel/login_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,6 +11,32 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start();
+    _userNameController
+        .addListener(() => _viewModel.setUsername(_userNameController.text));
+
+    _passwordController
+        .addListener(() => _viewModel.setUsername(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
